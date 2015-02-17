514dcb2b025081a9ffd2a9304a99dfc2a3bc2e97
Toby Birkett <tobyrbirkett@gmail.com>
2015-02-17 22:30:43 +0000

    added a class for the Student object added a class for an array list of students

---
+++ src/pracoursework/Student.java
@@ -0,0 +1,26 @@
+package pracoursework;
+
+public class Student {
+
+	private String studentFName;
+	private String studentSName;
+	private String studentNumber;
+	private String studentEmail;
+
+	public String getFName(){
+		return studentFName;
+	}
+	
+	public String getSName(){
+		return studentSName;
+	}
+	
+	public String getNumber(){
+		return studentNumber;
+	}
+	
+	public String getEmail(){
+		return studentEmail;
+	}
+	
+}
---
+++ src/pracoursework/studentList.java
@@ -0,0 +1,13 @@
+package pracoursework;
+
+import java.util.ArrayList;
+
+public class studentList {
+static ArrayList<Student> studentList = new ArrayList<Student>();
+
+public void addStudent(Student currentStudent){
+	studentList.add(currentStudent);
+}
+
+
+}
