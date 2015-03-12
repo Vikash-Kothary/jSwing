ff746e4594d0c93cb11dbe0fdf3ff6953207d986
Vikash Kothary <kothary.vikash@gmail.com>
2015-03-12 17:10:13 +0000

    interim saving

--- src/PRA/MainProgram.java
+++ src/PRA/MainProgram.java
@@ -1,10 +1,10 @@
 package PRA;
 
-public class mainProgram {
+public class MainProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Bitbucket sucks");
-
+		InterfaceWindow gui = new InterfaceWindow();
 	}
 
 }
